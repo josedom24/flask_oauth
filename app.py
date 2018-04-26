6ab882a6b07246dd056723ce931186b3f537df67
josedom24 <josedom24@gmail.com>
2018-04-26 19:28:07 +0200

    cambio a sesiones

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from urllib.parse import parse_qs
 import os,json
 app = Flask(__name__)   
 
+app.SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
 AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
 ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
